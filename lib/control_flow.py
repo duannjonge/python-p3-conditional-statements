d5c5368557f10dcaaa0f32553ef811c6a58793f8
duan spot <brandennjonge@gmail.com>
2023-08-23 13:27:12 +0300

    all tests passing

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -3,10 +3,10 @@
 def admin_login(username, password):
     # your code here
   
-    if username =="admin"or username =="ADMIN" and password =="12345":
-        print("Access granted")
+    if (username =="admin"or username =="ADMIN") and password =="12345":
+        return("Access granted")
     else:
-        print("Access denied")
+        return("Access denied")
 
 
 admin_login("admin","12345")
@@ -19,13 +19,13 @@ admin_login("ADMIN", "12345")
 def hows_the_weather(temperature):
     # your code here
     if temperature < 40:
-        print("It's brisk out there!")
-    elif temperature <=40 and temperature <=65:
-      print("It's a little chilly out there!")
+        return("It's brisk out there!")
+    elif temperature >=40 and temperature <=65:
+      return("It's a little chilly out there!")
     elif temperature >85:
-        print("It's too dang hot out there!")
+        return("It's too dang hot out there!")
     else:
-        print( "It's perfect out there!")
+        return( "It's perfect out there!")
 
 hows_the_weather(33)
 hows_the_weather(99)
@@ -37,15 +37,15 @@ def fizzbuzz(num):
     # your code here
     
     if num%5==0 and num%3==0:
-        print("FizzBuzz")
+        return("FizzBuzz")
     elif  num%3 ==0:
      
-        print("Fizz")
+        return("Fizz")
     elif num%5 ==0:
-        print("Buzz")
+        return("Buzz")
     
     else:
-        print(num)
+        return(num)
 
 fizzbuzz(1)
 fizzbuzz(2)
@@ -56,15 +56,16 @@ fizzbuzz(15)
 def calculator(operation, num1, num2):
     # your code here
     if operation== "+":
-        print(num1+num2)
+        return(num1+num2)
     elif operation== "-":
-        print(num1-num2)
+        return(num1-num2)
     elif operation == "*":
-        print(num1*num2)
+        return(num1*num2)
     elif operation =="/":
-        print(num1/num2)
+        return(num1/num2)
     else:
         print("Invalid operation!")
+        return None
 
 
 calculator("+", 1, 1)
